362ac79cb99109f5e0ea1fadebf9206f71d7ac63
shreyas23sk <shreyas23.sk@gmail.com>
2024-02-15 23:28:14 +0530

    add hashtable implementation

--- lexerHashTable.c
+++ lexerHashTable.c
@@ -4,8 +4,34 @@
 #endif
 
 pairLexemeToken createPairLexemeToken(char* s, TOKEN tk) {
-    pairLexemeToken newPair = (pairLexemeToken) malloc(sizeof(pairLexemeToken));
+    pairLexemeToken newPair = (pairLexemeToken) malloc(sizeof(struct pairLexemeToken));
     newPair->lexeme = s;
     newPair->val = tk;
     return newPair;
+}
+
+hashTable createEmptyHashTable() {
+    hashTable newTable = (hashTable) malloc(sizeof(struct hashTable));
+    newTable->size = 0;
+    return newTable;
+}
+
+int lookup(hashTable ht, char* s) {
+    for(int i = 0; i < ht->size; i++) {
+        if(strcmp(ht->items[i]->lexeme, s) == 0) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int insert(hashTable ht, pairLexemeToken plt) {
+    if(lookup(ht, plt->lexeme) < 0) {
+        ht->size++;
+        ht->items = (pairLexemeToken*) realloc(ht->items, sizeof(pairLexemeToken) * (ht->size));
+        ht->items[ht->size - 1] = plt; 
+        return 1;
+    } else {
+        return 0;
+    }
 }
--- lexerHashtable.h
+++ lexerHashtable.h
@@ -8,13 +8,15 @@ struct pairLexemeToken {
     char* lexeme;
     TOKEN val;
 };
-typedef struct pairLexemeToken* pairLexemeToken
+typedef struct pairLexemeToken* pairLexemeToken;
 
-typedef struct hashTable {
+struct hashTable {
     pairLexemeToken* items;
     int size;
-} hashTable;
+};
+typedef struct hashTable* hashTable;
 
 pairLexemeToken createPairLexemeToken(char*, TOKEN);
+hashTable createEmptyHashTable();
 int lookup(hashTable, char*); // return -1 if string s not found, else return index of entry
-int insert(hashTable, pairLexemeToken);
+int insert(hashTable, pairLexemeToken); // returns 1 if new pair successfully inserted, 0 if entry already present
