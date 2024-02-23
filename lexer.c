6b220f98e5995fa2ab93b3724a28dc0f84845909
Harshith Vasireddy <harshithvasireddy2004@gmail.com>
2024-02-23 22:38:32 +0530

    windows momint

--- lexer.c
+++ lexer.c
@@ -84,45 +84,7 @@ pairLexemeToken acceptState(token tk, twinBuffer B)
     return createPairLexemeToken(getLexeme(B), tk);
 }
 
-pairLexemeToken getNextToken()
-{
-    B = initBuffer(filename);
-    int state = 0;
-    char *lex;
-    while (1)
-    {
-        char c = nextChar(B);
-        switch (state)
-        {
-        case 0:
-            switch (c)
-            {
-            case '>':
-                state = 8;
-                break;
-            }
-            break;
-        case 8:
-            switch (c)
-            {
-            case '=':
-                state = 9;
-                break;
-            default:
-                state = 10;
-                break;
-            }
-            break;
-        case 9:
-            return acceptState(TK_GE, B);
-        case 10:
-            retract(B);
-            return acceptState(TK_GT, B);
-        }
-    }
-}
-
-pairLexemeToken getNextToken2() {
+pairLexemeToken getNextToken() {
     B = initBuffer(filename);
     int state = 0;
     char *lex;
