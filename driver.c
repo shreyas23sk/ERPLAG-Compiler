719882846b11a283ca6b076384810e59282802bb
shreyas23sk <shreyas23.sk@gmail.com>
2024-03-05 14:35:27 +0530

    add group member names

--- driver.c
+++ driver.c
@@ -1,3 +1,10 @@
+// Group No 37
+// Shreyas Kumar 2021A7PS2432P
+// Prateek Kashyap 2021A7PS1449P
+// Harshith Vasireddy 2021A7PS0504P
+// Yuvraj Doshi 2021A7PS2425P
+
+
 #ifndef PARSERDEF
 #define PARSERDEF
 #include "parserDef.h"
--- lexerDef.h
+++ lexerDef.h
@@ -1,3 +1,9 @@
+// Group No 37
+// Shreyas Kumar 2021A7PS2432P
+// Prateek Kashyap 2021A7PS1449P
+// Harshith Vasireddy 2021A7PS0504P
+// Yuvraj Doshi 2021A7PS2425P
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
